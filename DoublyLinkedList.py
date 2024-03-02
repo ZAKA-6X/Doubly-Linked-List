1d82c8a52ddaa83ca4726811621f9baf13d1f26c
ZAKA-6X <89934286+ZAKA-6X@users.noreply.github.com>
2024-03-02 23:20:51 +0100

    Add files via upload

---
+++ DoublyLinkedList.py
@@ -0,0 +1,30 @@
+class node:
+    def __init__(self, data,) -> None:
+        self.data = data 
+        self.next = None
+        self.previous = None
+
+class DoublyLinkedList:
+    def __init__(self) -> None:
+        self.head = None
+
+    def append(self, data):
+        if self 
+
+
+
+    def prepand(self, data):
+        pass
+
+    def display(self):
+        pass
+
+
+DLL = DoublyLinkedList()
+DLL.append(56)
+DLL.append(23)
+DLL.append(0)
+DLL.append(44)
+
+DLL.display()
+
